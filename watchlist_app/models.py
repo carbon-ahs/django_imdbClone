ba769827ef3b5339b543d9f5f0dc4b3eac40e346
carbon-ahs <shehanuk.ahsan@gmail.com>
2021-07-26 01:46:52 -0700

    going to add user

--- watchlist_app/models.py
+++ watchlist_app/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.db.models.deletion import CASCADE
 from django.utils.translation import activate
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django.contrib.auth.models import User
 
 class StreamPlatform(models.Model):
     name = models.CharField(max_length=50)
